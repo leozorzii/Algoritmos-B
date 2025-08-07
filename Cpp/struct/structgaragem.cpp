18d9b2401f607430b76f57a29af755bb4023ad12
Leo Zorzi <l.zorzi@ufn.edu.br>
2025-08-07 19:18:06 +0000

    projeto garagem em cpp

--- Cpp/struct/structgaragem.cpp
+++ Cpp/struct/structgaragem.cpp
@@ -1,91 +1,137 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <cctype>
 
 #define TAM 5
-
 using namespace std;
 
-typedef struct {
+struct Carro {
     string placas;
     string horaEntrada;
     string horaSaida;
-    float valor;
+    float valor{};
+};
 
-} Carro;
+        //  functions de validação
 
-int main(){
+bool validaPlaca7(const string& p) {
+    if (p.size() != 7) return false;
+    // 3 letras
+    for (int i = 0; i < 3; ++i) {
+        if (!isalpha((unsigned char)p[i])) return false;
+    }
+    // 4 números
+    for (int i = 3; i < 7; ++i) {
+        if (!isdigit((unsigned char)p[i])) return false;
+    }
+    return true;
+}
+
+bool validaHoraHHMM(const string& h) {
+    if (h.size() != 5) return false;
+    if (h[2] != ':') return false;
+    if (!isdigit((unsigned char)h[0]) || !isdigit((unsigned char)h[1]) ||
+        !isdigit((unsigned char)h[3]) || !isdigit((unsigned char)h[4])) return false;
+
+    int HH = (h[0] - '0') * 10 + (h[1] - '0');
+    int MM = (h[3] - '0') * 10 + (h[4] - '0');
+
+    if (HH < 0 || HH > 23) return false;
+    if (MM < 0 || MM > 59) return false;
+    return true;
+}
+
+
+int main() {
     Carro garagem[TAM];
-    Carro carro;
     int quantidadeCarros = 0;
-    int opcao = 1;
+    int opcao = 0;
 
-
-
-
-    
-    while(opcao != 4){
-        cout << "M E N U\n";
-        cout << " 1 - Veículo Entra\n";
-        cout << "2 - Veículo Sai\n";
-        cout << "3 - Listar Veículo\n";
+    while (opcao != 4) {
+        cout << "\n===== M E N U =====\n";
+        cout << "1 - Veiculo Entra\n";
+        cout << "2 - Veiculo Sai\n";
+        cout << "3 - Listar Veiculos\n";
         cout << "4 - Fim\n";
-        cout << opcao;
+        cout << "Escolha: ";
         cin >> opcao;
 
-        switch (opcao){
-        case 1:
-        cout << "entrando na garagem\n";
-        if(quantidadeCarros == TAM){
-            cout << "garagem cheia\n";
-        }else{
-            bool jaEstaNaGaragem = false;
-            for(int i=0; i<quantidadeCarros; i++){
-                if(garagem[i].placas == carro.placas){
-                    jaEstaNaGaragem = true;
-                    cout << "carro ja esta na garagem\n";
-                    break;
+        switch (opcao) {
+        case 1: {
+            cout << "\n--- Entrada de Veiculo ---\n";
+            if (quantidadeCarros == TAM) {
+                cout << "Garagem cheia!\n";
+                break;
+            }
+
+            Carro novo;
+            // placa válida
+            while (true) {
+                cout << "Placa (AAA0000): ";
+                cin >> novo.placas;
+                for (auto &c : novo.placas) c = toupper((unsigned char)c);
+
+                if (!validaPlaca7(novo.placas)) {
+                    cout << "Formato invalido. Use 3 letras + 4 numeros (ex.: ABC1234).\n";
+                    continue;
+                }
+
+                bool existe = false;
+                for (int i = 0; i < quantidadeCarros; ++i) {
+                    if (garagem[i].placas == novo.placas) { 
+                        existe = true; 
+                        break; 
+                    }
+                }
+                if (existe) {
+                    cout << "Este carro ja esta na garagem.\n";
+                    continue;
+                }
+                break;
+            }
+
+            // Hora válida
+            while (true) {
+                cout << "Hora de entrada (HH:MM): ";
+                cin >> novo.horaEntrada;
+                if (!validaHoraHHMM(novo.horaEntrada)) {
+                    cout << "Hora invalida. Use HH:MM de 00:00 a 23:59.\n";
+                    continue;
+                }
+                break;
+            }
+
+            garagem[quantidadeCarros++] = novo;
+            cout << "Ticket emitido. Carro " << novo.placas
+                 << " entrou as " << novo.horaEntrada << ".\n";
+            break;
+        }
+
+        case 2:
+            cout << "\n--- Saida de Veiculo ---\n";
+            break;
+
+        case 3:
+            cout << "\n--- Carros na Garagem ---\n";
+            if (quantidadeCarros == 0) {
+                cout << "Garagem vazia.\n";
+            } else {
+                for (int i = 0; i < quantidadeCarros; ++i) {
+                    cout << "Placa: " << garagem[i].placas
+                         << " | Entrada: " << garagem[i].horaEntrada << "\n";
                 }
             }
-            
-            cout << "placa: ";
-            cin >> carro.placas;
-            cout << "hora de entrada: ";
-            cin >> carro.horaEntrada;
-
-            garagem[ quantidadeCarros ] = carro;
-            quantidadeCarros++;
-            cout <<"imprimindo ticket\n";
-                    break;
-        }
-         case 2:
-        cout << "saindo da garagem\n";
-            break;
-        
-             case 3:
-        cout << "Listagem de carros na garagem\n";
-        if(quantidadeCarros == 0){
-            cout << "garagem vazia\n";
-        }else{
-            for(int i = 0; i < quantidadeCarros; i++){
-                cout << "carro com placa" << garagem[i].placas << "hora entrada" << garagem[i].horaEntrada << "\n";
-            }
-        }
             break;
 
-         case 4:
-        cout << "obrigado por usar o sistema\n";
+        case 4:
+            cout << "Encerrando.\n";
             break;
-        
-            
+
         default:
-        cout << "Opção inválida, tente novamente\n";
+            cout << "opcao invalida, tente novamente.\n";
             break;
         }
-    
     }
 
-    return 1;
-
-
+    return 0;
 }
-//verificar se o carro já está na garagem
